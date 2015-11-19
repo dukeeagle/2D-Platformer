f1655db0b6e0a0fa8e84fbfd7410fd34b85c6786
Luke Igel <luke.igel@gmail.com>
2015-11-18 18:19:45 -0600

    Adds Menu State Manager and some other delicious goodies

---
+++ src/com/lukeigel/game/main/GameState.java
@@ -0,0 +1,19 @@
+package com.lukeigel.game.main;
+
+import java.awt.Graphics;
+
+public abstract class GameState {
+	
+	protected GameStateManager gsm;
+	
+	public GameState(GameStateManager gsm){
+		this.gsm = gsm;
+		init();
+	}
+	//all of this methods will be required in the game state, but not necessarily used
+	public abstract void init();
+	public abstract void tick();
+	public abstract void draw(Graphics g);
+	public abstract void keyPressed(int k);
+	public abstract void keyReleased(int k);
+}
